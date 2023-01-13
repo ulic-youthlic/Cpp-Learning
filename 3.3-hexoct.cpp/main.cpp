b40cf1f1a1067932c2f9fa531453992133aa3a42
ulic-youthlic <ulic.youthlic@gmail.com>
2023-01-13 22:37:58 +0800

    A normal commit. I have go to 3.13.

---
+++ 3.3-hexoct.cpp/main.cpp
@@ -0,0 +1,19 @@
+// shows hex and octal literals
+
+#include <iostream>
+
+int main(void)
+{
+	using namespace std;
+
+	int chest = 42;
+	int waist = 0x42;
+	int inseam = 042;
+
+	cout << "Monsieur cuts a striking figure!\n";
+	cout << "chest = " << chest << " (42 in decimal)\n";
+	cout << "waist = " << waist << " (0x42 in hex)\n";
+	cout << "inseasm = " << inseam << " (042 in octal)\n";
+
+	return 0;
+}
---
+++ 3.4-hexoct2.cpp/main.cpp
@@ -0,0 +1,21 @@
+// display values in hex and octal
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int chest = 42;
+	int waist = 42;
+	int inseam = 42;
+
+	cout << "Monsieur cuts a striking figure!" << endl;
+	cout << "chest = " << chest << " (decimal for 42)" << endl;
+	cout << hex; //munipulator for chenging number base
+	cout << "waist = " << waist << " (hexademical for 42)" << endl;
+	cout << oct;
+	cout << "inseam = " << inseam << " (octal for 42)" << endl;
+
+	return 0;
+}
