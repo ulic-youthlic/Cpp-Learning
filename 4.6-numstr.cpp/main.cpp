59582b6d3e6ed6e273bdaaa6ae977a055d16f391
ulic-youthlic <ulic.youthlic@gmail.com>
2023-01-26 12:56:50 +0800

    A commit in linux

---
+++ 4.6-numstr.cpp/main.cpp
@@ -0,0 +1,20 @@
+// following number input with line input
+
+#include <iostream>
+
+int main(void)
+{
+	using namespace std;
+
+	cout << "What year was your house built?\n";
+	int year;
+	cin >> year;
+	cout << "What is its street adress?\n";
+	char address[80];
+	cin.getline(address, 80);
+	cout << "Year built: " << year << endl;
+	cout << "Address: " << address << endl;
+	cout << "Done!\n";
+
+	return 0;
+}
